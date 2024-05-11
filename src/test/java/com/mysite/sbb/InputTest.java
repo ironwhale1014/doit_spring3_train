5ee96e9ea95737aa506d04038fad7b1319be53bf
King <mac@mac.com>
2024-05-12 08:15:19 +0900

    3-2

---
+++ src/test/java/com/mysite/sbb/InputTest.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb;
+
+
+import com.mysite.sbb.question.QuestionService;
+import jakarta.validation.constraints.NotEmpty;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InputTest {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    void testJpa() {
+        for (int i = 0; i < 300; i++) {
+            String subject = String.format("테스트 데이터입니다.:[%03d]", i);
+            String content = "내용 없음 ";
+            questionService.create(subject, content);
+        }
+    }
+}
